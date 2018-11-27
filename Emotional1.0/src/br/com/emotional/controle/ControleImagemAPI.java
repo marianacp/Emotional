ed7aaeebb5b6a5121a33f41ba13e14479ecb65c5
Condina <viniciuscondina@gmail.com>
2018-11-26 23:29:13 -0200

    Lógica de Emoção

--- Emotional1.0/src/br/com/emotional/controle/ControleImagemAPI.java
+++ Emotional1.0/src/br/com/emotional/controle/ControleImagemAPI.java
@@ -9,21 +9,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.emotional.API.APIFace;
+import util.Erro;
 
 @WebServlet({"/detectarEmocao"})
 public class ControleImagemAPI extends HttpServlet {
 	protected void service(HttpServletRequest request,
             HttpServletResponse response)
             throws IOException, ServletException {
-		
+	Erro erro = new Erro(); 	
+	String URL;
 	String nome = request.getParameter("nome"); 	
 	String imagem = request.getParameter("imagem");
 	
 	APIFace apif = new APIFace();
 	apif.insereImagem(imagem);
-	apif.detectar();
+	String sentimento	= apif.detectar();
 	
-	  String URL = "DetectarEmocao.jsp";
+	
+	if (!sentimento.isEmpty()) {
+		 URL = "DetectarEmocao.jsp";
+		 request.setAttribute("sentimento", sentimento);
+	}
+	else {
+		erro.add("Não foi possível reconhecer o humor. Tente novamente!");
+		 URL = "DetectarEmocao.jsp";
+	}
+	
+	  request.setAttribute("mensagens", erro);
       RequestDispatcher rd = request.getRequestDispatcher(URL);
       rd.forward(request, response);
 	}
