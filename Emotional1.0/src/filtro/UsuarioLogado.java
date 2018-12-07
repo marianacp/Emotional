381f05852983c9f3edf6cd444e0f597c2088cfa6
Mariana Pinheiro <mariana.pinheiro@uniriotec.br>
2018-12-07 02:09:55 -0200

    Update

--- Emotional1.0/src/filtro/UsuarioLogado.java
+++ Emotional1.0/src/filtro/UsuarioLogado.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 /* 
  * Classe responsavel pela autenticação de usuario logado
  */
