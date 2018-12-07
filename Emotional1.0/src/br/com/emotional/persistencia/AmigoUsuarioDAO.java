ca072167f07c1d494cc11ff9e65d1fc2673caf20
MaluLopes93 <34509753+MaluLopes93@users.noreply.github.com>
2018-12-07 06:59:16 -0200

    javadoc

--- Emotional1.0/src/br/com/emotional/persistencia/AmigoUsuarioDAO.java
+++ Emotional1.0/src/br/com/emotional/persistencia/AmigoUsuarioDAO.java
@@ -7,7 +7,9 @@ import java.util.Calendar;
 
 import br.com.emotional.entidade.AmigoUsuario;
 import br.com.emotional.entidade.Usuario;
-
+/*
+ * Classe que aplica lógica de adicionar amigo no banco de dados
+ */
 public class AmigoUsuarioDAO extends Dao{
 	public void salvarSolicAmizade(AmigoUsuario amigo) throws Exception {
 		open(); 
